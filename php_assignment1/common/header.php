f12a9eb6b548bdcca968598316f0a4ddebc758e4
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-03-21 01:58:49 -0400

    Add files via upload

---
+++ php_assignment1/common/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header-align-wrap">
+        <div class="logo">
+            <p class="logo-text">Pineapple Music</p>
+        </div>
+        <nav class="menu-wrap">
+            <ul>
+                <li class="menu">
+                    <a href="/" style="margin-right: 30px;">Home</a>
+                    <a href="add.php" style="margin-right: 50px;">Add a Song</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
